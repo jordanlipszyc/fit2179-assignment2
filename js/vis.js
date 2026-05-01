e28578e49e9912ab6e368ea013ef1060f614752d
Jordan Lipszyc <jlip0008@student.monash.edu.au>
2026-05-01 14:52:45 +1000

    Further structure added with some filler elements

---
+++ js/vis.js
@@ -0,0 +1,5 @@
+// Standard Vega-Lite embedding code
+var spec = "js/vis.json";
+vegaEmbed('#vis', spec).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
