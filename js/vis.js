6e4aaac30628aa7d74cd5de663d7b508bc00519e
Jordan Lipszyc <jlip0008@student.monash.edu.au>
2026-05-01 15:30:29 +1000

    Added basic styling and grid restructuring

--- js/vis.js
+++ js/vis.js
@@ -1,5 +1,14 @@
 // Standard Vega-Lite embedding code
 var spec = "js/vis.json";
-vegaEmbed('#vis', spec).then(function(result) {
+
+vegaEmbed("#vis", spec)
+  .then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
+  })
+  .catch(console.error);
+
+vegaEmbed("#big-vis", spec)
+  .then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  })
+  .catch(console.error);
