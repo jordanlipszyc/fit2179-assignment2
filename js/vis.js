a7aeea86b5bf11e8cecb4f0942b91235ba709589
Jordan Lipszyc <jlip0008@student.monash.edu.au>
2026-05-13 08:18:42 +1000

    Remove attempt to Vega embed vis.js

--- js/vis.js
+++ js/vis.js
@@ -1,14 +1,7 @@
 // Standard Vega-Lite embedding code
-var spec = "js/vis.json";
-var spec2 = "js/influenza.json";
+var spec = "js/influenza.json";
 
-vegaEmbed("#vis", spec, { "actions": false })
-  .then(function (result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-  })
-  .catch(console.error);
-
-vegaEmbed("#big-vis", spec2, { "actions": false })
+vegaEmbed("#big-vis", spec, { "actions": false })
   .then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   })
