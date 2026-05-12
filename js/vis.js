06365eb494520c070645ab5f7e368ba97bcccd30
Jordan Lipszyc <jlip0008@student.monash.edu.au>
2026-05-12 19:32:01 +1000

    Added influenza data and created a basic choropleth vis

--- js/vis.js
+++ js/vis.js
@@ -1,13 +1,14 @@
 // Standard Vega-Lite embedding code
 var spec = "js/vis.json";
+var spec2 = "js/influenza.json";
 
-vegaEmbed("#vis", spec)
+vegaEmbed("#vis", spec, { "actions": false })
   .then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   })
   .catch(console.error);
 
-vegaEmbed("#big-vis", spec)
+vegaEmbed("#big-vis", spec2, { "actions": false })
   .then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   })
